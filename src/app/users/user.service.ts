b65f272fd61155807fe7d63fb8ea00d0ac54c487
marcos <marcos@Mac-mini-de-marcos.local>
2017-04-11 18:06:56 -0400

    prepare heroku config

--- src/app/users/user.service.ts
+++ src/app/users/user.service.ts
@@ -5,13 +5,14 @@ import 'rxjs/add/operator/toPromise';
 import {User} from './user';
 
 @Injectable()
-export class UserService{
+export class UserService {
   private headers = new Headers({'Content-Type': 'application/json'});
   private usersUrl = 'http://localhost:3000/users';
 
-  constructor(private http: Http){}
+  constructor(private http: Http) {
+  }
 
-  getUsers(): Promise<User[]>{
+  getUsers(): Promise<User[]> {
     return this.http.get(`${this.usersUrl}.json`)
       .toPromise()
       .then(response => response.json() as User[])
@@ -26,12 +27,32 @@ export class UserService{
       .catch(this.handleError);
   }
 
+  create(first_name: string, middle_name: string, last_name: string): Promise<User> {
+    return this.http
+      .post(`${this.usersUrl}.json`, JSON.stringify({
+        first_name: first_name,
+        middle_name: middle_name,
+        last_name: last_name
+      }), {headers: this.headers})
+      .toPromise()
+      .then(res => res.json().data as User)
+      .catch(this.handleError);
+  }
+
   update(user: User): Promise<User> {
     const url = `${this.usersUrl}/${user.id}.json`;
     return this.http
       .put(url, JSON.stringify(user), {headers: this.headers})
       .toPromise()
       .then(() => user)
+      .catch(this.handleError);
+  }
+
+  delete(id: number): Promise<void> {
+    const url = `${this.usersUrl}/${id}.json`;
+    return this.http.delete(url, {headers: this.headers})
+      .toPromise()
+      .then(() => null)
       .catch(this.handleError);
   }
 
--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {Router} from "@angular/router";
-import { Http } from '@angular/http';
+import {Http} from '@angular/http';
 
 import {User} from "./user";
 import {UserService} from "./user.service";
@@ -17,8 +17,8 @@ export class UsersComponent implements OnInit {
 
   constructor(private userService: UserService,
               private router: Router,
-              private http: Http
-  ) {}
+              private http: Http) {
+  }
 
   getUsers(): void {
     this.userService
@@ -26,11 +26,37 @@ export class UsersComponent implements OnInit {
       .then(users => this.users = users);
   }
 
+  add(first_name: string, middle_name: string, last_name: string): void {
+    first_name = first_name.trim();
+    middle_name = middle_name.trim();
+    last_name = last_name.trim();
+
+    if (!first_name && !middle_name && !last_name) {
+      return;
+    }
+    this.userService.create(first_name, middle_name, last_name)
+      .then(user => {
+        this.users.push(user);
+        this.selectedUser = null;
+      });
+  }
+
+  delete(user: User): void {
+    this.userService
+      .delete(user.id)
+      .then(() => {
+        this.users = this.users.filter(h => h !== user);
+        if (this.selectedUser === user) {
+          this.selectedUser = null;
+        }
+      });
+  }
+
   ngOnInit() {
     this.getUsers();
     /*
-    this.http.get('http://localhost:3000/users.json')
-      .subscribe(res => this.users = res.json());*/
+     this.http.get('http://localhost:3000/users.json')
+     .subscribe(res => this.users = res.json());*/
   }
 
   onSelect(user: User): void {
